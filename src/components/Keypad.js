a52774acce35ba5ac866f0f0be5402e77b461dca
Leroy-netizen <leroythomas051@gmail.com>
2022-07-17 17:13:30 +0300

    commit tree 3499a9af7575c39420a64ec128a5397ff085a617

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,19 @@
 // Code Keypad Component Here
+import React from "react";
+
+// methods
+const changeHandler = (e) => console.log("Entering password...");
+
+const Keypad = () => {
+  return (
+    <div>
+      <input
+        type="password"
+        placeholder="Enter Password"
+        onChange={changeHandler}
+      />
+    </div>
+  );
+};
+
+export default Keypad;
